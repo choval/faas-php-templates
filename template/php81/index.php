818828ae97d25f8aa9123212a68e6e16a6816f4a
Osvaldo Jiang <choval@gmail.com>
2023-01-22 10:50:34 -0300

    PHP8.1 template

---
+++ template/php81/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * This file loads the composer autoload and
+ * then requires the function's handler.php
+ */
+require(__DIR__.'/function/vendor/autoload.php');
+require(__DIR__.'/function/src/handler.php');
