2d973cf314cbbd694a9b8047b24b966a962ce98f
Osvaldo Jiang <choval@gmail.com>
2023-01-22 13:06:02 -0300

    Exit code fix

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ docker build -t faas-$TEMPLATE template/$TEMPLATE && \
 docker run --rm -p 8080:8080 -p 8081:8081 -d --name $TEMPLATE faas-$TEMPLATE && \
 curl http://localhost:8080/ -s | grep Hello && \
 curl http://localhost:8081/metrics -s -o /dev/null && \
-docker stop -t 1 $TEMPLATE || \
-docker stop -t 1 $TEMPLATE && exit 1
+(docker stop -t 1 $TEMPLATE; exit 0) || \
+(docker stop -t 1 $TEMPLATE; exit 1)
